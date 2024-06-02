6426ba166ed8ecb2b181999d7c031c59839b07ce
Costinand <trash_co@mail.ru>
2024-06-02 20:34:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fake_math import divide as f_d
 from true_math import divide as t_d
 
-num_ar = [57, 6], [14, 0]
-for i in num_ar:
-    first = i[0]
-    second = i[1]
-    f_d(first, second)
-    t_d(first, second)
+result1 = f_d(63, 9)
+result2 = f_d(14, 0)
+result3 = t_d(42, 5)
+result4 = t_d(19, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
 
