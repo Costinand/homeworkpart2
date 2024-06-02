19bf320bb882150e2dfbad3669b30d3d8f0890a2
Costinand <trash_co@mail.ru>
2024-06-02 20:33:23 +0200

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,15 +1,8 @@
 def divide(first, second):
 
     if second == 0:
-        print('(', first, '/',  second, ') = ', 'Ошибка')
+
+        return 'ошибка'
+
     else:
-        result = first / second
-        print('(', first, '/',  second, ') = ', result),
-
-#import random
-#for i in range (5):
-    #first = random.randint(1, 10)
-
-    #second = random.randint(0, 4)
-
-    #divide(first, second)
+        return first / second
