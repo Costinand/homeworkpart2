6e80d1b5954ee95d432f2846bee8e55491b87b58
Costinand <trash_co@mail.ru>
2024-06-02 20:34:18 +0200

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -1,18 +1,13 @@
-import math
+from math import inf
 
 
 def divide(first, second):
 
     if second == 0:
-        print('(', first, '/',  second, ') = ', math.inf)
+
+        return inf
     else:
-        result = first / second
-        print('(', first, '/',  second, ') = ', result),
+        return first / second
 
-#import random
-#for i in range (5):
-    #first = random.randint(1, 10)
 
-    #second = random.randint(0, 5)
 
-    #divide(first, second)
